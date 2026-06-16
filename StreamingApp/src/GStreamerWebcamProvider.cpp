f60989a2fb058eb781f2d0fc60ae2863f086446e
idzu9 <dimka.zui98@gmail.com>
2026-06-16 20:58:10 +0300

    improved eyes detection and added smile detection

--- StreamingApp/src/GStreamerWebcamProvider.cpp
+++ StreamingApp/src/GStreamerWebcamProvider.cpp
@@ -217,6 +217,9 @@ GstFlowReturn GStreamerWebcamProvider::_OnCameraFrameRecieved(GstElement* Sink, 
 				Face detection part
 			*/
 			std::string cascade_path = "../../../../StreamingApp/resources/haarcascades/haarcascade_frontalface_default.xml";
+			std::string eye_path = "../../../../StreamingApp/resources/haarcascades/haarcascade_eye.xml";
+			std::string smile_path = "../../../../StreamingApp/resources/haarcascades/haarcascade_smile.xml";
+
 
 			cv::CascadeClassifier faceCascade;
 			if (!faceCascade.load(cascade_path))
@@ -225,27 +228,82 @@ GstFlowReturn GStreamerWebcamProvider::_OnCameraFrameRecieved(GstElement* Sink, 
 				return GST_FLOW_OK;
 			}
 
+			cv::CascadeClassifier eyeCascade;
+			if (!eyeCascade.load(eye_path))
+			{
+				std::cerr << "Error: Could not load cascade classifier XML file." << std::endl;
+				return GST_FLOW_OK;
+			}
+
+			cv::CascadeClassifier smileCascade;
+			if (!smileCascade.load(smile_path))
+			{
+				std::cerr << "Error: Could not load cascade classifier XML file." << std::endl;
+				return GST_FLOW_OK;
+			}
+
 			std::vector<cv::Rect> faces;
-			faceCascade.detectMultiScale(Frame, faces, 1.1, 4, 0, cv::Size(30, 30));
+			std::vector<cv::Rect> eyes;
+			std::vector<cv::Rect> smiles;
+
+			faceCascade.detectMultiScale(Frame, faces, 1.1, 4, 0, cv::Size(20, 20));
 
 			cv::Mat BluredFrame;
 			cv::GaussianBlur(Frame, BluredFrame, cv::Size(51, 51), 0);
 
 
+			/*
+				adding background and me there in small square
+			*/
+			//cv::Mat background = cv::imread("../../../../StreamingApp/resources/backgrounds/background.png");
 
-			cv::Mat background = cv::imread("../../../../StreamingApp/resources/backgrounds/background.png");
+			//cv::Mat ResizedBackground;
+			//cv::resize(background, ResizedBackground, Frame.size());
 
-			cv::Mat ResizedBackground;
-			cv::resize(background, ResizedBackground, Frame.size());
+			//cv::Mat FrameCopy = Frame.clone();
 
-			cv::Mat FrameCopy = Frame.clone();
-
-			ResizedBackground.copyTo(Frame);
+			//ResizedBackground.copyTo(Frame);
 
 			for (const auto& face : faces)
 			{
 				cv::rectangle(Frame, face, cv::Scalar(0, 255, 0), 3);
 
+				smileCascade.detectMultiScale(Frame(face), smiles, 1.8, 22);
+
+				for (const auto& smile : smiles)
+				{
+					cv::rectangle(Frame(face), smile, cv::Scalar(0, 0, 255), 3);
+				}
+
+				eyeCascade.detectMultiScale(Frame(face), eyes, 1.1, 22);
+
+				for (const auto& eye : eyes)
+				{
+					cv::rectangle(Frame(face), eye, cv::Scalar(255, 0, 0), 3);
+
+
+					/*
+						1. Blur only face
+					*/
+					//cv::Mat faceROI = FrameCopy(face);
+					//cv::GaussianBlur(faceROI, faceROI, cv::Size(51, 51), 0);
+
+					/*
+						2. Blur everything except face
+					*/
+					//cv::Mat faceROI = Frame(face);
+					//cv::Mat targetROI = BluredFrame(face);
+					//faceROI.copyTo(targetROI);
+					//BluredFrame.copyTo(Frame);
+					//faceROI.copyTo(Frame);
+
+					/*
+						put my face on top of background
+					*/
+					//cv::Mat faceROI = FrameCopy(face);
+					//cv::Mat targetROI = Frame(face);
+					//faceROI.copyTo(targetROI);
+				}
 
 				/*
 					1. Blur only face
@@ -265,12 +323,10 @@ GstFlowReturn GStreamerWebcamProvider::_OnCameraFrameRecieved(GstElement* Sink, 
 				/*
 					put my face on top of background
 				*/
-				cv::Mat faceROI = FrameCopy(face);
-				cv::Mat targetROI = Frame(face);
-				faceROI.copyTo(targetROI);
+				//cv::Mat faceROI = FrameCopy(face);
+				//cv::Mat targetROI = Frame(face);
+				//faceROI.copyTo(targetROI);
 			}
-
-
 
 			cv::putText(Frame,
 				"WebRTC Stream Live",
